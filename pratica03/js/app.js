1bfe0800377e2d4df3bdaafbb1d8e6e7a5cffe4a
Camila  lima <92176387+Camilalima84@users.noreply.github.com>
2022-07-04 15:17:30 -0300

    Add files via upload

---
+++ pratica03/js/app.js
@@ -0,0 +1,14 @@
+let novaJanela;
+
+function abrirJanela() {
+    novaJanela = window.open("","novaJanela", "width=200,height=200");
+    novaJanela.document.write("<p>Uma nova janela foi criada</p>");
+
+}
+function fecharJanela() {
+novaJanela.close();
+}
+
+function nomeNavigation(){
+    alert(screen.height);
+}
